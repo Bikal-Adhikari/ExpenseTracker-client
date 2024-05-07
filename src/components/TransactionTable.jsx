2168cb81c85521f01185359052fea74be11110c0
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-05-07 15:18:08 +1000

    Delete transactions functionality

--- src/components/TransactionTable.jsx
+++ src/components/TransactionTable.jsx
@@ -1,18 +1,73 @@
 import Table from "react-bootstrap/Table";
 import { useUser } from "../UserContext";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { deleteTrans } from "../helpers/axiosHelper";
+import { toast } from "react-toastify";
 
 export const TransactionTable = () => {
   const { transactions, getUserTransactions } = useUser();
+  const [idsToDelete, setIdsToDelete] = useState([]);
   useEffect(() => {
     getUserTransactions();
   }, []);
+
+  const handleOnCheckBox = (e) => {
+    const { checked, value } = e.target;
+    console.log(checked, value);
+    if (value === "all") {
+      checked
+        ? setIdsToDelete(transactions.map((trans) => trans._id))
+        : setIdsToDelete([]);
+      return;
+    }
+    if (checked) {
+      setIdsToDelete([...idsToDelete, value]);
+    } else {
+      setIdsToDelete(idsToDelete.filter((id) => id !== value));
+    }
+  };
+
   const total = transactions?.reduce((acc, item) => {
     return item.type === "income" ? acc + item.amount : acc - item.amount;
   }, 0);
+
+  const handleOnDelete = async () => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete ${idsToDelete.length} many transactions`
+      )
+    ) {
+      const { status, message } = await deleteTrans(idsToDelete);
+      toast[status](message);
+      if (status === "success") {
+        getUserTransactions();
+        setIdsToDelete([]);
+      }
+    }
+  };
   return (
     <>
-      <div>{transactions?.length} Transactions found!</div>
+      <div className="d-flex justify-content-between">
+        <div>{transactions?.length} Transactions found!</div>
+        <Button
+          variant="danger"
+          disabled={idsToDelete.length < 1}
+          onClick={handleOnDelete}
+        >
+          Delete {idsToDelete.length} Transactions
+        </Button>
+      </div>
+
+      <div>
+        <Form.Check
+          onChange={handleOnCheckBox}
+          value="all"
+          type="checkbox"
+          label="Select All"
+          checked={transactions.every((item) => idsToDelete.includes(item._id))}
+        />
+      </div>
       <Table striped bordered hover>
         <thead>
           <tr>
@@ -25,7 +80,15 @@ export const TransactionTable = () => {
         <tbody>
           {transactions?.map((item) => (
             <tr key={item._id}>
-              <td>{item.date.slice(0, 10)}</td>
+              <td>
+                <Form.Check
+                  type="checkbox"
+                  label={item.date.slice(0, 10)}
+                  onChange={handleOnCheckBox}
+                  checked={idsToDelete.includes(item._id)}
+                  value={item._id}
+                />
+              </td>
               <td>{item.title}</td>
               {item.type === "income" ? (
                 <>
--- src/helpers/axiosHelper.js
+++ src/helpers/axiosHelper.js
@@ -82,3 +82,25 @@ export const fetchTrans = async () => {
     };
   }
 };
+export const deleteTrans = async (idsToDelete) => {
+  try {
+    const userId = getUserId();
+    if (!userId) {
+      throw new Error("User id doesn't exist! Login and try again");
+    }
+
+    const { data } = await axios.delete(transEP, {
+      data: idsToDelete,
+      headers: {
+        Authorization: userId,
+      },
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+    return {
+      status: "error",
+      message: error.message,
+    };
+  }
+};
